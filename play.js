b61b8a69b3c21739e988521d88925da2f36553d4
Kevinliao0857 <kevinliao0857@gmail.com>
2021-02-10 23:20:26 +0000

    more snek stuff

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-
-
-const setupInput = () => {
-
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on(handleUserInput);
-
-  return stdin;
-}
-
-const handleUserInput = (key) => {
-  if(key === '\u0003') {
-    process.exit();
-  }
-};
-
-console.log('after callback');
-
+setupInput();
